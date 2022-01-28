c16566410e09d413bdeeee3a0a41d75dc246fbd9
matheusyanmonteiro <matheusyanmonteiro@gmail.com>
2022-01-28 17:34:17 -0300

    connection with postgres test

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,13 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
     UsersModule,
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'db',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true
-    })
+    DatabaseModule
   ],
   controllers: [AppController],
   providers: [AppService],
